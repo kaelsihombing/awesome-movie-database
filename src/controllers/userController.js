4abf1228130fa0bdf82063147e89b5ab7df338d0
michaelsihombing <sntmcl1@gmail.com>
2020-02-27 22:10:55 +0700

    [MIKE]

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.js');
-const mailer = require('../helpers/nodeMailer');
-const bcrypt = require('bcryptjs');
+// const mailer = require('../helpers/nodeMailer');
+// const bcrypt = require('bcryptjs');
 const {
     success,
     error,
@@ -57,65 +57,41 @@ exports.recover = async (req, res) => {
 }
 
 exports.reset = async (req, res) => {
-
-    User.findOne({ sessionToken: req.params.token, sessionTokenExpires: { $gt: Date.now() } })
-        .then(user => {
-            if (!user) return res.render('expiredToken');
-
-            //Redirect user to form with the email address
-            res.render('reset', { user });
-        })
-        .catch(err => res.status(500).json({ message: err.message }));
+    try {
+        await User.reset(req)
+        res.render('reset');
+    }
+    catch (err) {
+        error(res, err, 422)
+    }
 };
 
 exports.resetPassword = async (req, res) => {
-
-    User.findOne({ sessionToken: req.params.token })
-        .then(async user => {
-            if (!user) return res.status(401).json({ message: 'Password reset token is invalid or has expired.' });
-
-            //Set the new password
-            user.encrypted_password = await bcrypt.hashSync(req.body.password, 10);
-            user.sessionToken = undefined;
-            user.sessionTokenExpires = undefined;
-
-            // Save
-            user.save((err) => {
-
-                if (err !== null) return res.status(500).json({ message: 'Error brooo' });
-
-                // send email
-                const mailOptions = {
-                    to: user.email,
-                    from: process.env.FROM_EMAIL,
-                    subject: "Your password has been changed",
-                    text: `Hi ${user.fullname} \n 
-                    This is a confirmation that the password for your account ${user.email} has just been changed.\n`
-                };
-
-                mailer.send(mailOptions, (error) => {
-                    if (error) return res.status(500).json({ message: error.message });
-                    res.render('done')
-                    // res.status(200).json({ message: 'Your password has been updated.' });
-                });
-            });
-        });
+    try {
+        await User.resetPassword(req)
+        res.render('done');
+    }
+    catch (err) {
+        error(res, err, 422)
+    }
 };
 
 exports.verifyEmail = async (req, res) => {
-
-    User.findOne({ sessionToken: req.params.token, sessionTokenExpires: { $gt: Date.now() } })
-        .then(user => {
-            if (!user) return res.render('expiredToken');
-
-            user.verified = true;
-            user.sessionToken = undefined;
-            user.sessionTokenExpires = undefined;
-
-            user.save(err => {
-                if (err !== null) return res.status(500).json({ message: 'Error brooo' });
-                res.status(500).json('Your account has been verified!' )
-            })
-        })
-        .catch(err => res.status(500).json({ message: err.message }));
+    try {
+        let result = await User.verifyEmail(req)
+        success(res, result, 422)
+    }
+    catch (err) {
+        error(res, err, 422)
+    }
 };
+
+exports.resentEmailVerification = async (req, res) => {
+    try {
+        let result = await User.resendEmail(req)
+        success(res, result, 201)
+    }
+    catch(err) {
+        error(res, err, 422)
+    }
+}
